cde0da07f386e7b5d7ff16eb425d492b8ca2e11d
Jiří Pešík <jiri.pesik@outlook.cz>
2023-09-14 23:27:41 +0200

    Příklady

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## Příklady
 
 * [Bonusové příklady na lekci 11. 9. 2023](priklady-lekce-2.ipynb)
+* [Bonusové příklady na lekci 16. 9. 2023](priklady-lekce-3.ipynb)
---
+++ solution/karty2.py
@@ -0,0 +1,27 @@
+from itertools import product
+import random
+
+def sort_function(value):
+    colour = value[0]
+    return {"H": 1/10, "T": 1/100, "C": 1/1000, "P": 1/10000}.get(colour) + int(value[1])
+
+
+for x in range(10):
+    colours = ["H", "T", "C", "P"]
+    values = [str(i) for i in range(2, 10)]
+
+    # Vytvoříme seznam všech karet
+    cards = list(product(colours, values))
+
+    all_players_cards = []
+
+    for i in range(3):
+        player_cards = []
+        for j in range(8):
+            index = random.randrange(len(cards))
+            random_value = "".join(cards.pop(index))
+            player_cards.append(random_value)
+        player_cards = sorted(player_cards, key=sort_function)
+        all_players_cards.append("-".join(player_cards))
+
+    print(f'["{",".join(all_players_cards)}"]'.replace(",", '", "'), end=",\n")
